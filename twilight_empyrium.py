b426df20b88247b52edf792addbabf319a147e32
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2018-01-19 22:37:22 -0500

    init commit
    I've stubbed out some functions and a test module

---
+++ twilight_empyrium.py
@@ -0,0 +1,23 @@
+""" Twilight Emperium 
+map generator
+game
+
+Start with a map generator
+Find a hex grid template
+Then maybe a generator for the tiles? 
+Or a straight up iterator, that'd be fun to practice. 
+"""
+
+def hex_tile_generator(n=29, tile_stack=None):
+	''' generates n tiles from list tilestack
+	'''
+	if tile_stack == None:
+		# set the default tile_stack
+		tile_stack = []
+	
+	
+
+if __name__ == '__main__':
+	''' get a generator
+	'''
+	te_gen = grid_tile_generator()
---
+++ twilight_empyrium_test.py
@@ -0,0 +1,7 @@
+''' twilight_empyrium.py test module
+'''
+import unittest
+
+class Test_twilight_empyrium(unittest.test):
+	
+	def test_hextile_generator(self):

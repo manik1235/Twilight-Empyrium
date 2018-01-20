7b4b9b8a515c3e372bfcc3f84cf9d57b6f431f8c
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2018-01-20 00:26:37 -0500

    lots of changes. created the basic hex tile generator with builtins

--- twilight_empyrium.py
+++ twilight_empyrium.py
@@ -8,16 +8,42 @@ Then maybe a generator for the tiles?
 Or a straight up iterator, that'd be fun to practice. 
 """
 
-def hex_tile_generator(n=29, tile_stack=None):
-	''' generates n tiles from list tilestack
-	'''
-	if tile_stack == None:
-		# set the default tile_stack
-		tile_stack = []
-	
-	
+import functools
+from random import SystemRandom as rand
+
+def hex_tile_generator(builtin='ordered5', n=None, tile_stack=None):
+    """ generates n tiles from list tilestack
+
+    hex_tile_generator(builtin='builtin1')
+    hex_tile_generator(builtin='builtin1')
+    hex_tile_generator(builtin='builtin1')
+
+
+
+    """
+    built_ins = {'builtin1': [2, 1, 4, 3, 5],
+                 'ordered5': [1, 2, 3, 4, 5],
+                 'random': rand.choice(set(range(1, n))),
+                 'ordered29': list(range(1,30)),
+                 }
+
+    if tile_stack == None:
+        # set the default tile_stack
+        tile_stack = built_ins['ordered5']
+
+    if built_ins is not None:
+        try:
+            output_list = built_ins[builtin]
+        except KeyError:
+            print('builtin not found. Defaulting to builtin "ordered5"')
+            output_list = built_ins['ordered5']
+    
+    for output in output_list:
+        yield output
+    
+    
 
 if __name__ == '__main__':
-	''' get a generator
-	'''
-	te_gen = grid_tile_generator()
+    """ get a generator
+    """
+    te_gen = grid_tile_generator()
--- twilight_empyrium_test.py
+++ twilight_empyrium_test.py
@@ -1,7 +1,24 @@
-''' twilight_empyrium.py test module
-'''
+""" twilight_empyrium.py test module
+"""
+# tabs for indentation for use with jota+ text editor on my phone            80
 import unittest
+import twilight_empyrium as te
+from functools import namedtuple
 
-class Test_twilight_empyrium(unittest.test):
-	
-	def test_hextile_generator(self):
+
+class Test_twilight_empyrium(unittest.TestCase):
+    """ Collection of tests for the twilight_empyrium game
+    """
+
+    def test_hex_tile_generator_known_answer(self):
+        Ans = namedtuple('Ans', ['input', 'ans'])
+        known_answers_hex_tile_generator = [
+            Ans('builtin1', [2, 1, 4, 3, 5]),
+            Ans('ordered5', [1, 2, 3, 4, 5]),
+            ]
+
+        for ans in known_answers_hex_tile_generator:
+            test_te = list(te.hex_tile_generator(ans.input))
+            self.assertListEqual(ans.ans, test_te)
+
+    def test_hex_tile_generator_random(self):
